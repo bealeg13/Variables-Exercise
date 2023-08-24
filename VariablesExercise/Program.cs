83ba65f64620ff8737218328a8f51f339e0e2a09
Gabriel Bealer <bealeg13@gmail.com>
2023-08-24 12:48:40 -0700

    first commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Henny";
+            int dogAge = 6;
+            char firstLetter = 'H';
+            bool isDog = true;
+            double dogPrice = 100;
+            decimal dogValue = 1000.00m;
+            
+            Console.WriteLine($"My dog's name is {dogName}. He is {dogAge} years old. " +
+                $"The first letter of his name starts with an {firstLetter}. It is {isDog} that he is a dog. " +
+                $"I paid ${dogPrice} for him, but the value he brings to my family exceeds ${dogValue}." );
         }
     }
 }
